75096aff39e39a95d66bf581b86e1fa273b76eca
Victor Soto <dev.victor.soto@gmail.com>
2025-09-23 15:45:29 -0500

    feat: make logo clickable to navigate to home page
    - Convert logo/header div to NavLink component
    - Enable navigation to home route (/) when clicking on logo
    - Improve user experience with intuitive logo navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,10 +7,10 @@ const Sidebar = () => {
   return (
     <div className='xl:col-span-2 lg:col-span-3 md:col-span-1 bg-gray-800 text-white p-4 min-h-full'>
       <div className='space-y-4'>
-        <div className='flex items-center space-x-2'>
+        <NavLink to='/' className='flex items-center space-x-2'>
           <img className='w-10 h-10' src={logo} alt='Logo' />
           <h1 className='text-lg font-semibold'>Administración</h1>
-        </div>
+        </NavLink>
         <nav className='space-y-2'>
           <NavLink to='/question' className='flex items-center space-x-2 text-gray-300 hover:text-white cursor-pointer'>
             <img className='w-5 h-5' src={questionMark} alt='questions-icon' />
